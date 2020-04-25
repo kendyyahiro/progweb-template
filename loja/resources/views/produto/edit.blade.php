cdd7c6d1101bd9dfb8748ccc0937eed252eb689f
kendyyahiro <yahirokendy@gmail.com>
2020-04-25 16:15:11 -0300

    Facilitamos o acesso.

--- loja/resources/views/produto/edit.blade.php
+++
@@ -1,9 +0,0 @@
-
-
-@extends('template.common.base')
-
-@section('conteudo')
-
-<h1>Editar do produto aqui</h1>
-
-@endsection
--- loja/resources/views/site/advertise-here.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('site.master.layout')
-
-@section('conteudo')
-	<h1>Anuncie Aqui</h1>
-@endsection()
