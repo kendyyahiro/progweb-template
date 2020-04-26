0f7a1d70d3f40af0f6e1550944370201611a2c8a
roger <rdr.roger@gmail.com>
2020-04-26 13:54:11 -0400

    register em portugues

--- loja-definitivo/public/js/app.js
+++ loja-definitivo/public/js/app.js
@@ -37393,8 +37393,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! c:\xampp\htdocs\progweb-template\loja-definitivo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! c:\xampp\htdocs\progweb-template\loja-definitivo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\progweb-template\loja-definitivo\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\progweb-template\loja-definitivo\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
