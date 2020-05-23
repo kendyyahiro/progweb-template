08891f070023a45da66baab1de6b06a017aa84df
Victor Koji <vitorkoji15@gmail.com>
2020-05-23 17:25:58 -0400

    Caso não tenha produtos no carrinho, mostrar mensagem.

--- loja-definitivo/resources/views/carrinho-compra/index.blade.php
+++ loja-definitivo/resources/views/carrinho-compra/index.blade.php
@@ -2,8 +2,9 @@
 
 @section('content')
 
+
 <div class="container">
-	<div class="catalogo">
+	<div id="carrinho-compra-listagem">
 		<h2>Meu carrinho</h2>
 		<table class="table">
 			<thead class="thead-dark">
@@ -15,25 +16,30 @@
 				</tr>
 			</thead>
 			<tbody>
-				@foreach ($produtos as $produto)
+				@if(count($produtos) != 0 )
+					@foreach ($produtos as $produto)
+						<tr>
+							<td scope="row">
+								<div class="foto">
+									<a href="{{ route('produto/show', $produto->id) }}"><img class="card-img-top" src="{{ asset($produto->imagem) }}" alt="{{ $produto->nome }}" alt="{{ $produto->nome }}"></a>
+								</div>
+							</td>
+							<td width="150px"><span class="nome_produto">{{ $produto->nome }}</span></td>
+							<td><p class="descricao_produto">{{ $produto->descricao }}</p></td>
+							<td><span class="valor_produto">R${{ number_format($produto->valor,2,",",".") }}</span></td>
+						</tr>
+					@endforeach
 					<tr>
-						<td scope="row">
-							<div class="foto">
-								<a href="{{ route('produto/show', $produto->id) }}"><img class="card-img-top" src="{{ asset($produto->imagem) }}" alt="{{ $produto->nome }}" alt="{{ $produto->nome }}"></a>
-							</div>
-						</td>
-						<td><span class="nome_produto">{{ $produto->nome }}</span></td>
-						<td><span class="descricao_produto">{{ $produto->descricao }}</span></td>
-						<td><span class="valor_produto">R${{ number_format($produto->valor,2,",",".") }}</span></td>
+						<td colspan="3"></td>
+						<td id="valor-total-carrinho"> Total: <?= number_format($valor[0]->total, 2, ",", ".") ?> </td>
 					</tr>
-				@endforeach
+				@else
 				<tr>
-					<td colspan="3"></td>
-					<td> <strong>Total: <?= number_format($valor[0]->total, 2, ",", ".") ?> </strong> </td>
+					<td colspan="4" class="text-center"><h2>Você ainda não possui produtos no carrinho!</h2></td>
 				</tr>
+				@endif
 			</tbody>
 		</table>
-		
 	</div>
 </div>
 
